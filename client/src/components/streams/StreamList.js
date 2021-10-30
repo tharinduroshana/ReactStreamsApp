44570ce4f080b74071afec747630d5f2d6ac6d33
HexTechSolutions <hextecsolutions@gmail.com>
2021-10-30 19:32:53 +0530

    Implemented createBrowserHistory to navigate

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
+import { Link } from "react-router-dom";
 
 class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
+  }
+
+  renderCreate() {
+    const { isSignedIn } = this.props;
+    if (isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
   }
 
   render() {
@@ -12,8 +26,21 @@ class StreamList extends React.Component {
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
+  }
+
+  renderAdmin(stream) {
+    const { currentUserId } = this.props;
+    if (stream.userId === currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
   }
 
   renderList() {
@@ -21,6 +48,7 @@ class StreamList extends React.Component {
     return streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          <div>{this.renderAdmin(stream)}</div>
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -32,9 +60,17 @@ class StreamList extends React.Component {
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  const { streams } = state;
-  return { ...state, streams: Object.values(streams) };
+const mapStateToProps = (state) => {
+  const {
+    streams,
+    auth: { userId, isSignedIn },
+  } = state;
+  return {
+    ...state,
+    streams: Object.values(streams),
+    currentUserId: userId,
+    isSignedIn,
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
